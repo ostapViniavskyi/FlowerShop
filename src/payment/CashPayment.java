bf7fec038e96595f7e88379cc276f541eec8c79a
ostapViniavskyi <viniavskyi@ucu.edu.ua>
2018-12-14 17:37:54 +0200

    Added Main class

--- src/payment/CashPayment.java
+++ src/payment/CashPayment.java
@@ -2,7 +2,7 @@ package payment;
 
 import cart.Cart;
 
-public class CashPayment {
+public class CashPayment implements PaymentStrategy {
     public void pay(Cart cart){
         System.out.printf("Paying %.2f in cash...", cart.getPrice());
     }
